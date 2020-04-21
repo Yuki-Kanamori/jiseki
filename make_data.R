0b86244f3b29c9c04dd15dbef5477352579629ba
Yuki-Kanamori <y.kanamory@gmail.com>
2020-04-21 11:48:30 +0900

    initial

---
+++ make_data.R
@@ -0,0 +1,9 @@
+
+# setting directory ---------------------------------------------
+setwd("/Users/Yuki/Dropbox/サンマ耳石")
+load("input_20_mat.rda")
+
+
+# packages ------------------------------------------------------
+require(imager)
+require(abind)
