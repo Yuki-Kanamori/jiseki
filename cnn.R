fb732e25a6b31bfac247f0a1ba3bb20e8bcc305a
Yuki-Kanamori <y.kanamory@gmail.com>
2020-04-24 16:59:54 +0900

    added model

--- cnn.R
+++ cnn.R
@@ -6,6 +6,12 @@ library(keras)
 setwd("/Users/Yuki/Dropbox/sokouo1/jiseki")
 load("testdata.RData")
 
+batch_size <- 256
+epochs <- 20
+validation_split <- 0.1
+img_size <- dim(data$figure)[-1]
+rep <- 1
+
 set.seed(0)
 nr = nrow(data$figure) #1185
 sprit = sample(nr, replace = F, nr*0.85)
@@ -16,3 +22,45 @@ data2 = list(data$figure[sprit,,,, drop=F], data$age[sprit, ])
 train = data$figure[sprit,,,, drop=F]
 test = data$figure[-sprit,,,, drop=F]
 
+model_age_f <- keras_model_sequential()
+model_age_f %>% 
+  layer_conv_2d(
+    filter = 32, kernel_size = c(3,3), padding = "same", 
+    input_shape = img_size
+  ) %>%
+  layer_activation("relu") %>%
+  layer_conv_2d(filter = 32, kernel_size = c(3,3)) %>%
+  layer_activation("relu") %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_conv_2d(filter = 64, kernel_size = c(3,3), padding = "same") %>%
+  layer_activation("relu") %>%
+  layer_conv_2d(filter = 64, kernel_size = c(3,3)) %>%
+  layer_activation("relu") %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_flatten() %>%
+  layer_dense(512) %>%
+  layer_activation("relu") %>%
+  layer_dropout(0.5) %>%
+  layer_dense(dim(y_train)[2]) %>%
+  layer_activation("softmax")
+
+model_age_f %>% compile(
+  loss = 'categorical_crossentropy',
+  optimizer = optimizer_rmsprop(),
+  metrics = c('accuracy')
+)
+
+history <- model_age_f %>% fit(
+  x_train, y_train,
+  batch_size = batch_size,
+  epochs = epochs,
+  verbose = 1,
+  callbacks = callback_tensorboard(log_dir = "logs/run_b"),
+  validation_split = validation_split,
+  shuffle = T
+)
+
+result <- model_age_f %>% predict(x_test)
+y_pred <- round(result, 0)
+mistake <- which(y_pred[,1]-y_test[,1]!=0)
+acc <- 1 - length(mistake)/nrow(y_test)
